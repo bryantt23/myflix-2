55d3804dbf973551a6db9406618c31e27e855d9d
Luke Tower <lukegtower@gmail.com>
2013-09-03 17:12:59 -0400

    Test category model. Test video model belongs to category and category has many videos

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Category do
+
+  it "saves itself" do
+    cat = Category.new(name: "Action")
+    cat.save
+    Category.first.name.should == "Action"
+  end
+
+  it "has many videos" do
+    cat = Category.new(name: "Action")
+    vid1 = Video.new(title: 'Commando')
+    vid2 = Video.new(title: 'Predator')
+    vid3 = Video.new(title: 'Mission Impossible')
+
+    cat.save
+    vid1.save
+    vid2.save
+    vid3.save
+
+    vid1.categories << cat
+    vid2.categories << cat
+    vid3.categories << cat
+
+    Category.first.videos.size.should == 3
+  end
+end
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Video do
+
+  it 'saves itself' do
+    video = Video.new(title: "Die Hard", description: "Action movie",
+      small_cover_url: "die_hard_small.jpg", large_cover_url: "die_hard_large")
+    video.save
+    Video.first.title.should == "Die Hard"
+  end
+
+  it 'belongs to a category' do
+    video = Video.new(title: "Die Hard", description: "Action movie",
+      small_cover_url: "die_hard_small.jpg", large_cover_url: "die_hard_large")
+    category = Category.new(name: "Action")
+    video.save
+    category.save
+
+    video = Video.first
+    category = Category.first
+    video.categories << category
+
+    Video.first.categories.first.name.should == "Action"
+  end
+end
