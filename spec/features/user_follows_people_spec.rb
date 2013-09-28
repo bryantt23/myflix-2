7000380318f2b40fc0d68c0c2e335168f8c82ebe
Luke Tower <lukegtower@gmail.com>
2013-09-28 19:31:45 -0400

    Updated feature spec for following users

--- spec/features/user_follows_people_spec.rb
+++ spec/features/user_follows_people_spec.rb
@@ -7,16 +7,23 @@ feature "People I Follow page" do
     joe = Fabricate(:user)
     comedies = Fabricate(:category, name: 'Comedies')
     video = Fabricate(:video, user_reviews: [Fabricate(:user_review, user_id: joe.id)], categories: [comedies])
+
     login(bob)
     visit root_path
     page.should have_content("Comedies")
+
     click_on "video_#{video.id}"
     page.should have_content("#{joe.full_name}")
+
     click_on "#{joe.full_name}"
     page.should have_button("Follow")
+
     click_on "Follow"
+    page.should have_content("People I Follow")
+
     click_on "People"
     page.should have_content("#{joe.full_name}")
+
     within(:xpath, "//tr[contains(.,'#{joe.full_name}')]") do
       find("a[href='/follows/#{joe.id}']").click
     end
