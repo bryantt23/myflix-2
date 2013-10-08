63413f6604db750b4b41929e2c419ceb40bd03dc
Luke Tower <lukegtower@gmail.com>
2013-10-07 20:14:00 -0400

    Fix bug in mailgun info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Myflix::Application.configure do
     address:              ENV["MAILGUN_SMTP_SERVER"],
     port:                 ENV["MAILGUN_SMTP_PORT"],
     domain:               'fierce-falls-6035.herokuapp.com',
-    user_name:            ENV["MAILGUN_SMTP_LOGIN"],,
-    password:             ENV["MAILGUN_SMTP_PASSWORD"],,
+    user_name:            ENV["MAILGUN_SMTP_LOGIN"],
+    password:             ENV["MAILGUN_SMTP_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
