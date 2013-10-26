665ee7df82edad58918f218cec8439c851bbd279
Luke Tower <lukegtower@gmail.com>
2013-10-25 21:48:34 -0400

    Move create customer to independent class. Write tests for create method

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -10,14 +10,10 @@ module StripeWrapper
     def self.create(options={})
       StripeWrapper.set_api_key
       begin
-        customer = Stripe::Customer.create(
-          card: options[:card],
-          plan: 'standard',
-          email: options[:email])
         response = Stripe::Charge.create(
                     amount: options[:amount],
                     currency: 'usd',
-                    customer: customer.id,
+                    card: options[:card],
                     description: options[:description])
         new(response, :success)
       rescue Stripe::CardError => e
@@ -36,5 +32,30 @@ module StripeWrapper
 
   def self.set_api_key
     Stripe.api_key = Rails.env.production? ? ENV['STRIPE_LIVE_SECRET_KEY'] : ENV['STRIPE_SECRET_KEY']
+  end
+
+  class Customer
+    attr_reader :response, :error_message
+
+    def initialize(options={})
+      @response = options[:response]
+      @error_message = options[:error_message]
+    end
+
+    def self.create(options={})
+      begin
+        response = Stripe::Customer.create(
+          card: options[:card],
+          plan: 'standard',
+          email: options[:user].email)
+        new(response: response)
+      rescue Stripe::CardError => e
+        new(error_message: e.message)
+      end
+    end
+
+    def successful?
+      response.present?
+    end
   end
 end
