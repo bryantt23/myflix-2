3104dfcbeaa9d6399fd831591ae3cf410300c369
Luke Tower <lukegtower@gmail.com>
2013-11-30 12:40:33 -0500

    update carrierwave config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AWS_ACCESS_KEY_ID',          # required
-      :aws_secret_access_key  => 'AWS_SECRET_ACCESS_KEY',      # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],          # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],      # required
     }
-    config.fog_directory  = 'luke-tower-myflix'                     # required
+    config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
