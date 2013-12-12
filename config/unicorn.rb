5acea400b99b892b655d9926fa799c7a4ec8d4ce
Luke Tower <lukegtower@gmail.com>
2013-12-12 13:19:36 -0500

    change procfile and unicorn.rb for heroku bckgrd jobs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,3 +21,18 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 end
+
+before_fork do |server, worker|
+     @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+end
+
+after_fork do |server, worker|
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
+end
+
